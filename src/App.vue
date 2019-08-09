27b5fe2d53460d334069bd96d856314167a70932
Dhruv <dhruvmisra@live.com>
2019-08-09 17:50:59 +0530

    Finalized the functionality. Added instructions after creating

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,17 @@
               v-model="author"
             />
           </div>
+          <div class="form-group col-12">
+            <label for="author">Main Image:</label>
+            <input
+              type="file"
+              accept="image/*"
+              class="col-10 mx-1"
+              :id="blogMainImage.imgId"
+              placeholder="Content"
+              @change="bindImage($event, blogMainImage)"
+            />
+          </div>
         </div>
 
         <div class="form-group col-12 mx-auto">
@@ -184,9 +195,15 @@
                 <div v-else-if="item.tag == 'a'" class="col-10 row">
                   <input
                     type="text"
-                    class="form-control col-10 mx-1"
+                    class="form-control col-12 mx-1"
                     placeholder="Content"
                     v-model="item.text"
+                  />
+                  <input
+                    type="text"
+                    class="form-control col-8 m-1"
+                    placeholder="Link"
+                    v-model="item.link"
                   />
                   <div class="custom-control custom-checkbox my-auto">
                     <input
@@ -245,9 +262,31 @@
         </div>
       </form>
 
-      <p>{{blogContent}}</p>
+      <!-- <p>{{blogContent}}</p> -->
 
       <button class="d-block btn btn-danger btn-lg px-5 mx-auto my-5" @click="createPage">Create</button>
+
+      <section v-if="created">
+        <div class="container">
+          <h6>Follow these steps to add the blog page: </h6>
+          <ol>
+            <li>Login to Cpanel by going to <a href="http://www.grynow.in/cpanel">http://www.grynow.in/cpanel</a>. </li>
+            <li>Open File Manager and open the 'public_html' directory.</li>
+            <li>Click the 'Upload' button and upload the zip file just downloaded. Close the upload page when completed.</li>
+            <li>Refresh the public_html directory to find the upload.zip file. Right click on the file and 
+              select 'Extract'. Click on 'Extract File(s)' button.</li>
+            <li>
+              Open 'blogs.html' and copy & paste the following code inside the blog list: 
+              <div class="col-sm-12 col-md-8 rounded-lg bg-secondary p-5 m-2">
+                <p class="text-white">
+                  {{ blogsCode }}
+                </p>
+              </div>
+            </li>
+          </ol>
+        </div>
+      </section>
+ 
     </div>
   </div>
 </template>
@@ -260,22 +299,24 @@ export default {
   name: "app",
   data() {
     return {
-      fileName: "blog",
+      created: false,
+      fileName: "",
       metaTitle: "",
       metaDescription: "",
       blogTitle: "",
       author: "",
       blogContent: [],
-      blogImages: [{
+      blogMainImage: {
         id: 0,
         file: null,
         tag: "img",
         imgId: 1
-      }],
+      },
       blogImagesId: 2,
       blogInputs: 1,
       pageContent: "",
-      zip: new JSZip()
+      blogsCode: '',
+      zip: null
     };
   },
   computed: {
@@ -288,11 +329,20 @@ export default {
   },
   methods: {
     addTextInput(tag, array) {
-      array.push({
-        id: this.blogInputs,
-        text: "",
-        tag: tag
-      });
+      if(tag == 'a') {
+        array.push({
+          id: this.blogInputs,
+          text: "",
+          link: '',
+          tag: tag
+        });
+      } else {
+        array.push({
+          id: this.blogInputs,
+          text: "",
+          tag: tag
+        });
+      }
       this.blogInputs++;
     },
     addLink(array) {
@@ -343,11 +393,11 @@ export default {
       this.blogInputs++;
     },
     bindImage(event, item) {
-      let file = event.target.files[0];
-      this.addImagesToZip(file);
-      let type = file.type;
-      let imgName = file.name;
-      let blob = new Blob([file], {type: type});
+      item.file = event.target.files[0];
+
+      // let type = file.type;
+      // let imgName = file.name;
+      // let blob = new Blob([file], {type: type});
       // console.log(blob);
       
       // let objectURL = window.URL.createObjectURL(blob);
@@ -360,12 +410,6 @@ export default {
       // link.click();
       // link.remove();
     },
-    addImagesToZip(file) {
-      this.zip.folder("assets").file('image.png', file);
-      // this.zip.generateAsync({type: "blob"}).then(function(content) {
-      //   FileSaver.saveAs(content, "upload.zip");
-      // });
-    },
     deleteInput(id, array) {
       const index = array.findIndex(item => {
         return item.id == id;
@@ -373,10 +417,22 @@ export default {
       array.splice(index, 1);
     },
     createPage() {
-      this.pageContent = `
-<!DOCTYPE html>
+      this.zip = new JSZip();
+
+      this.pageContent = `<!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <scr` + `ipt async src="https://www.googletagmanager.com/gtag/js?id=UA-143978554-1"></scr` + `ipt>
+  <scr` + `ipt>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      
+      gtag('config', 'UA-143978554-1');
+      gtag('config', 'AW-721210182');
+  </scr` + `ipt>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -384,17 +440,49 @@ export default {
   <title>${this.metaTitle}</title>
 	<link rel="icon" type="image/ico" href="../assets/logo.png" />
   <meta name="description" content="${this.metaDescription}">
+
+  <!-- Canonical tag -->
+  <link rel="canonical" href="http://www.grynow.in/blog/${this.fileNameWithExtention}" />
   
   <!-- Bootstrap -->
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<!-- Custom Bootstrap -->
 	<link rel="stylesheet" type="text/css" href="../css/custom-bootstrap.css">
-
-	<!-- AOS -->
-	<link rel="stylesheet" href="../css/aos.css" />
 	<!-- Custom CSS -->
 	<link rel="stylesheet" type="text/css" href="../css/styles.css">
   <link rel="stylesheet" type="text/css" href="../css/styles-blog.css">
+
+  <!--Start of Tawk.to Script-->
+  <scr` + `ipt type="text/javascript">
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("scr` + `ipt")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/5cfd335b267b2e5785318955/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+  </scr` + `ipt>
+  <!--End of Tawk.to Script-->	
+  
+  <!-- Facebook Pixel Code -->
+  <scr` + `ipt>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2357150577939123');
+    fbq('track', 'PageView');
+  </scr` + `ipt>
+  <noscr` + `ipt><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=2357150577939123&ev=PageView&noscript=1"
+  /></noscr` + `ipt>
+  <!-- End Facebook Pixel Code -->
 </head>
 <body>
 
@@ -477,9 +565,9 @@ export default {
             this.pageContent += `
               <a `;
             if(item.nofollow) {
-              this.pageContent += `rel="nofollow `;
+              this.pageContent += `rel="nofollow" `;
             }
-            this.pageContent += `href="${item.text}">${item.text}</a>`;
+            this.pageContent += `href="${item.link}">${item.text}</a>`;
             break;
           }
           case 'ul': {
@@ -538,11 +626,142 @@ export default {
               </ol>`;
               break;
           }
+          case 'img': {
+            this.pageContent += `
+            <img src="../assets/blogs/${this.fileName}${item.imgId}.jpg" alt="${this.fileName}-${item.imgId}" class="w-100 mt-3">`;
+          }
         }
       }
+      //Author
       this.pageContent += `
         <span class="author">- ${this.author}</span>`;
-      console.log(this.pageContent);
+
+      this.pageContent += `
+  </div>
+
+      <div class="my-5 py-5 text-center">
+        <h5>Similar blogs:</h5>
+        <ul style="list-style-type: none">
+          <li><a href="http://www.grynow.in/blog/influencer-marketing-in-india.html">Influencer Marketing in India: How Brands can grow with it</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- footer -->
+	<footer class="footer">
+		<div class="container">
+			<div class="social-row">
+				<p class="text-white text-center">Follow Us</p>
+				<div class="d-flex justify-content-center">
+					<a rel="nofollow" href="https://www.youtube.com/channel/UChCwo_Lrp1pljzDhevETeGw" target="_blank">
+						<img src="../assets/social icons/youtube.png" alt="" class="social">
+					</a>
+					<a rel="nofollow" href="https://www.instagram.com/gry_now" target="_blank">
+						<img src="../assets/social icons/instagram.png" alt="" class="social">
+					</a>
+					<a rel="nofollow" href="https://facebook.com/grynow" target="_blank">
+						<img src="../assets/social icons/facebook.png" alt="" class="social">
+					</a>
+					<a rel="nofollow" href="https://www.linkedin.com/company/gry/" target="_blank">
+						<img src="../assets/social icons/linkedin.png" alt="" class="social">
+					</a>
+					<a rel="nofollow" href="https://twitter.com/Grynowagency" target="_blank">
+						<img src="../assets/social icons/twitter.png" alt="" class="social">
+					</a>
+				</div>
+			</div>
+			<br>
+			<hr style="background-color: white">
+			<br>
+			<div class="col-md-12 col-lg-4 text-white row">
+				<img src="../assets/logo_with_title.png" 
+					alt="GryNow logo" 
+					class="brand-icon">
+				</div>
+			<br>
+			<div class="row text-white">
+				<div class="col-md-12 col-lg-4">
+					<ul style="list-style-type: none;">
+						<li class="list-title">About</li>
+						<li class="list-element">GryNow is the leading platform for brands, looking for influencers/creators, 
+							to market their products.</li>
+					</ul>
+				</div>
+				<div class="col-xs-3 col-lg-4">
+					<ul style="list-style-type: none;">
+						<li class="list-title">Navigate</li>
+						<li><a class="list-link" href="../social-media-influencer-platform-and-market-place.html">CREATORS</a></li>
+						<li><a class="list-link" href="../brand-marketing-management-agency.html">BRANDS & AGENCIES</a></li>
+						<li><a class="list-link" href="../case-studies.html">CASE STUDIES</a></li>
+						<li><a class="list-link" href="../blogs.html">BLOGS</a></li>
+						<li><a class="list-link" href="../videos-social-media-content-by-grynow.html">VIDEOS</a></li>
+						<li><a class="list-link" href="../influencer-management-agency-india.html">ABOUT US</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-3 col-lg-4">
+					<ul style="list-style-type: none;">
+						<li class="list-title">Contact Us</li>
+						<li>820, Golf Course Road</li>
+						<li>Sector 42, Gurugram</li>
+						<li>Haryana - 122001, India</li>
+						<br>
+						<li>Email: <a class="list-link" href="mailto:aman@grynow.in">aman@grynow.in</a></li>
+						<li>Contact: <a class="list-link" href="tel:9917486405">+91 9917486405</a></li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+		<div class="copyright">&copy; 2017-2019 GryNow | All Rights Reserved</div>
+  </footer>
+  
+  <!-- Bootstrap -->
+	<scr` + `ipt src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+	crossorigin="anonymous"></scr` + `ipt>
+	<scr` + `ipt src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+	integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+	crossorigin="anonymous"></scr` + `ipt>
+	<scr` + `ipt src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+	integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+	crossorigin="anonymous"></scr` + `ipt>
+
+
+</body>
+</html>`;
+
+      this.blogsCode = `<div class="card blog col-sm-12 col-md-6 col-lg-3">
+					<a class="blog-link" href="./blog/${this.fileNameWithExtention}">			
+						<div class="blog-image">
+							<img src="./assets/blogs/${this.fileName}1.jpg" alt="${this.fileName}" class="w-100">
+						</div>
+						<div class="card-body">
+							<h4 class="card-title">${this.blogTitle}</h4>
+							<br>
+							<span>${this.author}</span>
+						</div>
+					</a>
+				</div>`;
+
+      this.zip.folder("blog").file(this.fileNameWithExtention, this.pageContent);
+
+      let name;
+      for(let i=0; i<this.blogContent.length; i++) {
+        if(this.blogContent[i].tag == 'img') {
+          console.log('inside loop');
+          name = this.fileName + this.blogContent[i].imgId + '.jpg';
+          this.zip.folder("assets").folder("blogs").file(name, this.blogContent[i].file);
+        }
+      }
+      name = this.fileName + this.blogMainImage.imgId + '.jpg';
+      this.zip.folder("assets").folder("blogs").file(name, this.blogMainImage.file);
+      
+      this.zip.generateAsync({type: "blob"}).then(function(content) {
+        FileSaver.saveAs(content, "upload.zip");
+        this.created = true;
+      });
     },
     downloadFile() {
       let file = new Blob([this.pageContent], { type: "text/plain" });
